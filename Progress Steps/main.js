8702cb1f4f2e2c3604b4e5c1956073c6b3b2e41e
Hatim-Fangour <hatimfangour87@gmail.com>
2023-09-01 20:46:10 +0100

    commit 1

---
+++ Progress Steps/main.js
@@ -0,0 +1,59 @@
+const prev = document.getElementById("prev")
+const next = document.getElementById("next")
+const progress = document.getElementById('progress')
+const steps = document.querySelectorAll('.step')
+const bgButtonNext = document.getElementById("next")[0]
+
+var currentStep = 1
+
+next.addEventListener("click", ()=>{
+    currentStep++;
+
+    if(currentStep > steps.length) {
+        currentActive = steps.length
+    }
+
+    update()
+    const actives = document.querySelectorAll('.active')
+    next.style.setProperty("--dynamic-width", (actives.length - 1) / (steps.length - 1)  * 100  + '%')
+    document.getElementById("next")[0].style.animationPlayState = "running";
+})
+
+prev.addEventListener("click", ()=>{
+    currentStep--;
+
+    if(currentStep < steps.length) {
+        currentActive = steps.length
+    }
+
+    update()
+})
+
+
+
+
+function update() {
+    steps.forEach((step, idx) => {
+
+        if(idx < currentStep) {
+            step.classList.add('active')
+        } else {
+            step.classList.remove('active')
+        }
+    })
+
+
+
+    const actives = document.querySelectorAll('.active')
+
+    progress.style.width = (actives.length - 1) / (steps.length - 1)  * 100  + '%'
+    
+    if(currentStep === 1) {
+        prev.disabled = true
+    } else if(currentStep === steps.length) {
+        next.disabled = true
+    } else {
+        prev.disabled = false
+        next.disabled = false
+    }
+}
